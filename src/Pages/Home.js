e9faa46ae7b80b50a3f26361b8936645e5524edc
Adam-Shaka <1909564@brunel.ac.uk>
2024-03-04 20:40:22 +0000

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,27 @@
 export default function Home() {
-    return (
-        <div>
-            <section className="section">
-                <div className="box-main">
-                    <h2>Data Vis Home Page</h2>
-                    <p>Welcome to data vis this part of my final year project has the aim to generate charts to help visualise data gathered using NDT's.</p>
-                    <p>The Line chart sections shows a visualisation of data collected in regards to two counter offensive periods during the Ukraine war</p>
-                    <p>The chart generation section should generate a line chart based of user input if the correct data is given</p>
-                    <p>The documentation section provides all documentation from how to gather the data to how to generate the line chart.</p>
-
-                </div>
-            </section>
+  return (
+    <div>
+      <section className="section">
+        <div className="box-main">
+          <h2>Data Vis Home Page</h2>
+          <p>
+            Welcome to data vis this part of my final year project has the aim
+            to generate charts to help visualise data gathered using NDT's.
+          </p>
+          <p>
+            The Line chart sections shows a visualisation of data collected in
+            regards to two counter offensive periods during the Ukraine war
+          </p>
+          <p>
+            The chart generation section should generate a line chart based of
+            user input if the correct data is given
+          </p>
+          <p>
+            The documentation section provides all documentation from how to
+            gather the data to how to generate the line chart.
+          </p>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
